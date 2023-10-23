33c076039b84bfa272d5bb79d9ae378ffdb30453
RXGabriel <gabriel2005rf@gmail.com>
2023-10-23 15:31:56 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,10 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int colums) {
+        if (rows < 1 || colums < 1) {
+            throw new RuntimeException("Error creating board, there must be at least 1 row and 1 column");
+        }
+
         this.rows = rows;
         this.colums = colums;
 
@@ -15,15 +19,15 @@ public class Board {
     public int getRows() {
         return rows;
     }
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
     public int getColums() {
         return colums;
     }
-    public void setColums(int colums) {
-        this.colums = colums;
+
+    public Piece piece(int rows,int colums){
+        return pieces[rows][colums];
     }
 
-
+    public Piece piece(Position position){
+        return piece(position.getRow(),position.getColumn());
+    }
 }
